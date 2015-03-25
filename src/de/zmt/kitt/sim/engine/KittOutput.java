bd7aec9ff3584d198aa18d643e382ee59d2c4e69
mey <christian.meyer@zmt-bremen.de>
2015-03-25 09:52:29 +0100

    added KittOutput.java     provides continuous output KittGui.java     added "Show Output Inspector" menu item displaying KittOutput simplified CombinedInspector.java

---
+++ src/de/zmt/kitt/sim/engine/KittOutput.java
@@ -0,0 +1,42 @@
+package de.zmt.kitt.sim.engine;
+
+import sim.display.GUIState;
+import sim.engine.*;
+import sim.portrayal.*;
+import sim.portrayal.inspector.ProvidesInspector;
+import de.zmt.sim.portrayal.inspector.CombinedInspector;
+
+/**
+ * Provides continuous output within the GUI via {@link Inspector} and file.
+ * 
+ * @author cmeyer
+ * 
+ */
+// TODO file output
+public class KittOutput implements Steppable, ProvidesInspector {
+    private static final long serialVersionUID = 1L;
+
+    private final Environment environment;
+
+    public KittOutput(Environment environment) {
+	this.environment = environment;
+    }
+
+    @Override
+    public void step(SimState state) {
+	// TODO calculate continuous output
+
+    }
+
+    public String getFubar() {
+	return "fubar";
+    }
+
+    @Override
+    public Inspector provideInspector(GUIState state, String name) {
+	return new CombinedInspector(new SimpleInspector(this, state, name),
+		Inspector.getInspector(environment, state,
+			Environment.class.getSimpleName()));
+    }
+
+}
