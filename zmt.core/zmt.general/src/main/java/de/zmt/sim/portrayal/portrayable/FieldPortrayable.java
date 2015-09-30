55a2c5aa06a2d77a21c1ce410b0458e169aee480
mey <christian.meyer@zmt-bremen.de>
2015-09-30 18:44:18 +0200

    FieldPortrayable.java     added type parameter specifying the returned field

--- zmt.core/zmt.general/src/main/java/de/zmt/sim/portrayal/portrayable/FieldPortrayable.java
+++ zmt.core/zmt.general/src/main/java/de/zmt/sim/portrayal/portrayable/FieldPortrayable.java
@@ -4,12 +4,14 @@ package de.zmt.sim.portrayal.portrayable;
  * Simple portrayable to provide data for portraying fields.
  * 
  * @author cmeyer
+ * @param <T>
+ *            type of field
  *
  */
-public interface FieldPortrayable extends Portrayable {
+public interface FieldPortrayable<T> extends Portrayable {
     /**
      * 
      * @return the field object
      */
-    Object getField();
+    T getField();
 }
