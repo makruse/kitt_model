c852f656e4dc330a3aaad56036c839250c268276
mey <christian.meyer@zmt-bremen.de>
2015-12-17 16:17:36 +0100

    renamed DefinitionSeparatedCollector.java to CategoryCollector     now independent from ParamDefinition     works with any kind of object as category

--- zmt.core/zmt.general/src/main/java/sim/engine/output/CategoryCollector.java
+++ zmt.core/zmt.general/src/main/java/sim/engine/output/CategoryCollector.java
@@ -4,15 +4,13 @@ import java.io.Serializable;
 import java.util.*;
 import java.util.logging.Logger;
 
-import sim.engine.params.def.ParamDefinition;
 import sim.util.Propertied;
 import sim.util.Properties;
 
 /**
  * Implementation for the {@link Collector} interface, discriminating between
- * parameter definitions. Dumps headers with {@link ParamDefinition}'s title as
- * prefix, in order to separate data from different groups of simulation
- * objects.
+ * categories. Dumps headers with category {@link #toString()} as prefix, in
+ * order to separate data from different groups of simulation objects.
  * <p>
  * Data is stored within an encapsulated map that is used directly for providing
  * {@link Properties}.
@@ -20,57 +18,57 @@ import sim.util.Properties;
  * @author mey
  * 
  * @param <K>
- *            the agent class' {@link ParamDefinition}
+ *            the category type
  * @param <V>
- *            map value type
+ *            the {@code Collectable} type stored for every category
  */
-public abstract class DefinitionSeparatedCollector<K extends ParamDefinition, V extends Collectable>
-	implements Collector, Propertied, Serializable {
+public abstract class CategoryCollector<K, V extends Collectable> implements Collector, Propertied, Serializable {
     @SuppressWarnings("unused")
-    private static final Logger logger = Logger.getLogger(DefinitionSeparatedCollector.class.getName());
+    private static final Logger logger = Logger.getLogger(CategoryCollector.class.getName());
     private static final long serialVersionUID = 1L;
 
     /**
-     * Separator in header between between definition title prefix and
-     * collectable's header.
+     * Separator in header between between category prefix and collectable's
+     * header.
      */
     private static final String SEPARATOR = "$";
 
-    private final Map<K, V> dataPerDefinition;
+    private final Map<K, V> collectablePerCategory;
     private final int columnCount;
     private Collectable collectable = new MyCollectable();
 
     /**
-     * Constructs a new {@code DefinitionSeparatedCollector}.
+     * Constructs a new {@code DefinitionSeparatedCollector}. Each given
+     * category will be associated with a {@code Collectable}.
      * 
-     * @param simObjectDefs
-     *            the parameter definitions, each of them associated with a
-     *            group of simulation objects
+     * @see #createCollectable(Object)
+     * @param categories
+     *            the categories
      */
-    public DefinitionSeparatedCollector(Collection<? extends K> simObjectDefs) {
+    public CategoryCollector(Collection<? extends K> categories) {
 	// use linked hash map to maintain key insertion order
-	dataPerDefinition = new LinkedHashMap<>();
+	collectablePerCategory = new LinkedHashMap<>();
 
 	int columnCount = 0;
-	for (K def : simObjectDefs) {
+	for (K def : categories) {
 	    V collectable = createCollectable(def);
-	    dataPerDefinition.put(def, collectable);
+	    collectablePerCategory.put(def, collectable);
 	    columnCount += collectable.getSize();
 	}
 	this.columnCount = columnCount;
     }
 
-    protected V getData(K definition) {
-	return dataPerDefinition.get(definition);
+    protected V getData(K category) {
+	return collectablePerCategory.get(category);
     }
 
     /**
      * 
-     * @param definition
+     * @param category
      * @return {@link Collectable} of type {@code K} that will be associated
-     *         with {@code definition}.
+     *         with {@code category}.
      */
-    protected abstract V createCollectable(K definition);
+    protected abstract V createCollectable(K category);
 
     @Override
     public void beforeCollect(BeforeMessage message) {
@@ -87,12 +85,12 @@ public abstract class DefinitionSeparatedCollector<K extends ParamDefinition, V 
 
     @Override
     public Properties properties() {
-	return Properties.getProperties(dataPerDefinition);
+	return Properties.getProperties(collectablePerCategory);
     }
 
     @Override
     public String toString() {
-	return dataPerDefinition.toString();
+	return collectablePerCategory.toString();
     }
 
     private class MyCollectable implements Collectable {
@@ -100,21 +98,18 @@ public abstract class DefinitionSeparatedCollector<K extends ParamDefinition, V 
 
 	@Override
 	public void clear() {
-	    for (Collectable data : dataPerDefinition.values()) {
+	    for (Collectable data : collectablePerCategory.values()) {
 		data.clear();
 	    }
 	}
 
-	/**
-	 * Obtain headers from collectables with the definition's title as
-	 * prefix.
-	 */
+	/** Obtain headers from collectables with the category's as prefix. */
 	@Override
 	public Collection<String> obtainHeaders() {
 	    Collection<String> headers = new ArrayList<>(columnCount);
-	    for (K key : dataPerDefinition.keySet()) {
-		for (String header : dataPerDefinition.get(key).obtainHeaders()) {
-		    headers.add(key.getTitle() + SEPARATOR + header);
+	    for (K key : collectablePerCategory.keySet()) {
+		for (String header : collectablePerCategory.get(key).obtainHeaders()) {
+		    headers.add(key + SEPARATOR + header);
 		}
 	    }
 
@@ -124,8 +119,8 @@ public abstract class DefinitionSeparatedCollector<K extends ParamDefinition, V 
 	@Override
 	public Collection<?> obtainData() {
 	    Collection<Object> data = new ArrayList<>(columnCount);
-	    for (K key : dataPerDefinition.keySet()) {
-		data.addAll(dataPerDefinition.get(key).obtainData());
+	    for (K key : collectablePerCategory.keySet()) {
+		data.addAll(collectablePerCategory.get(key).obtainData());
 	    }
 
 	    return data;
