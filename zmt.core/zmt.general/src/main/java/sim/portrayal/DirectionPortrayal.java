8efc0b8c93aebd010d747f46a4e775647cf44b0e
mey <christian.meyer@zmt-bremen.de>
2016-03-02 10:51:25 +0100

    DirectionPortrayal.java: directions are skipped     if scale is too small to draw all     slightly increased dot size

--- zmt.core/zmt.general/src/main/java/sim/portrayal/DirectionPortrayal.java
+++ zmt.core/zmt.general/src/main/java/sim/portrayal/DirectionPortrayal.java
@@ -13,41 +13,87 @@ import sim.util.Double2D;
 /**
  * Draws directions using lines with dots at their ends. Directions are
  * represented by {@link Double2D} objects within an {@link ObjectGrid2D}.
+ * <p>
+ * If the scale is too small to draw all directions, every nth direction is
+ * skipped, while n decreases when scale increases.
  * 
  * @author mey
  *
  */
 public class DirectionPortrayal extends SimplePortrayal2D {
     private static final long serialVersionUID = 1L;
+
     private static final Paint DIRECTION_PAINT = Color.RED;
-    private static final double MINIMUM_DRAW_SIZE = 8;
+    /** Minimum draw scale in which all directions are drawn. */
+    private static final double MINIMUM_FULL_DRAW_SCALE = 8;
+    /** Scale of a line related to the size of a square. */
     private static final double LINE_SCALE = 1 / 2d;
-    private static final double DOT_SCALE = 1 / 8d;
+    /** Scale of a dot related to the size of a square. */
+    private static final double DOT_SCALE = 1 / 6d;
 
     @Override
     public void draw(Object object, Graphics2D graphics, DrawInfo2D info) {
+	double x = info.draw.x;
+	double y = info.draw.y;
 	double w = info.draw.width;
 	double h = info.draw.height;
 
-	// check if zoom level is appropriate
-	if (w < MINIMUM_DRAW_SIZE || h < MINIMUM_DRAW_SIZE || !info.clip.intersects(info.draw)) {
+	if (isSkipped(x, w) || isSkipped(y, h)) {
 	    return;
 	}
 
 	Double2D direction = (Double2D) object;
-	double x1 = info.draw.x;
-	double y1 = info.draw.y;
-	double x2 = x1 + direction.x * w * LINE_SCALE;
-	double y2 = y1 + direction.y * h * LINE_SCALE;
+	drawDirection(graphics, x, y, w, h, direction);
+    }
 
+    /**
+     * Checks if a direction should be skipped. The more draw scale is below
+     * {@value #MINIMUM_FULL_DRAW_SCALE}, the more directions are skipped.
+     * 
+     * @param position
+     *            the position of the direction to be drawn
+     * @param scale
+     *            the draw scale
+     * @return <code>true</code> if drawing of this direction is skipped
+     */
+    private static boolean isSkipped(double position, double scale) {
+	if (scale <= MINIMUM_FULL_DRAW_SCALE) {
+	    double gap = (MINIMUM_FULL_DRAW_SCALE / scale);
+
+	    // positions are centered, need to subtract 0.5
+	    if ((position / scale - 0.5) % gap != 0) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    /**
+     * Draws direction at given coordinates. Directions are represented as a dot
+     * in the center with a line pointing into the direction.
+     * 
+     * @param graphics
+     * @param x
+     * @param y
+     * @param w
+     * @param h
+     * @param direction
+     */
+    private static void drawDirection(Graphics2D graphics, double x, double y, double w, double h, Double2D direction) {
+	// do not let the directions be painted too small
+	double drawWidth = Math.max(MINIMUM_FULL_DRAW_SCALE, w);
+	double drawHeight = Math.max(MINIMUM_FULL_DRAW_SCALE, h);
+	double x2 = x + direction.x * drawWidth * LINE_SCALE;
+	double y2 = y + direction.y * drawHeight * LINE_SCALE;
+
+	graphics.setPaint(DIRECTION_PAINT);
 	// draw each direction as a line with a dot for direction
-	Line2D line = new Line2D.Double(x1, y1, x2, y2);
+	Line2D line = new Line2D.Double(x, y, x2, y2);
 	graphics.draw(line);
 
 	Ellipse2D dot = new Ellipse2D.Double();
-	dot.setFrameFromCenter(x2, y2, x2 + w * DOT_SCALE, y2 + h * DOT_SCALE);
+	dot.setFrameFromCenter(x, y, x + drawWidth * DOT_SCALE, y + drawHeight * DOT_SCALE);
 	graphics.fill(dot);
-	graphics.setPaint(DIRECTION_PAINT);
     }
 
     /**
