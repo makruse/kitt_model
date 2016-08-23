c619a19529476fbaada2f34d1c006ff47824c79b
Christian Meyer <christian.meyer@leibniz-zmt.de>
2016-08-23 08:36:56 +0200

    KittTest.java: checks serialization via checkpointing

--- src/test/java/sim/engine/KittTest.java
+++ src/test/java/sim/engine/KittTest.java
@@ -1,9 +1,6 @@
 package sim.engine;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.Before;
@@ -12,7 +9,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import de.zmt.params.KittParams;
-import test.util.SerializationUtil;
+import de.zmt.params.SpeciesDefinition;
 
 public class KittTest {
     private Kitt state;
@@ -24,18 +21,35 @@ public class KittTest {
     public void setUp() throws Exception {
         state = new Kitt();
         state.setParams(new KittParams());
-        state.setOutputPath(folder.newFolder().toPath());
+        state.setOutputPath(folder.newFolder("output").toPath());
+
+        // use only one fish to make test faster
+        state.getParams().getSpeciesDefs().stream().forEach(
+                definition -> ((SpeciesDefinition.MyPropertiesProxy) definition.propertiesProxy()).setInitialNum(1));
     }
 
     @Test
     public void serialization() throws IOException, ClassNotFoundException {
         state.start();
+        File startCheckpoint = folder.newFile();
+        state.writeToCheckpoint(startCheckpoint);
+
         state.schedule.step(state);
-        byte[] objData = SerializationUtil.write(state);
-        Object restoredSim = SerializationUtil.read(objData);
-        assertThat(restoredSim, is(instanceOf(Kitt.class)));
+        File stepCheckpoint = folder.newFile();
+        state.writeToCheckpoint(stepCheckpoint);
+
         state.schedule.step(state);
         state.finish();
+
+        testFromCheckpoint(startCheckpoint, folder.newFolder("start_output"));
+        testFromCheckpoint(stepCheckpoint, folder.newFolder("step_output"));
+    }
+
+    private static void testFromCheckpoint(File startCheckpoint, File outputFolder) {
+        Kitt restoredStartState = (Kitt) SimState.readFromCheckpoint(startCheckpoint);
+        restoredStartState.setOutputPath(outputFolder.toPath());
+        restoredStartState.schedule.step(restoredStartState);
+        restoredStartState.finish();
     }
 
 }
