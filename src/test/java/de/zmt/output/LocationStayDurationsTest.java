21b1356ea61fa9526bbbf8c2cbbce0cf6ed37df9
Christian Meyer <christian.meyer@leibniz-zmt.de>
2016-11-02 15:07:09 +0100

    LocationStayDurations.java: fixed test

--- src/test/java/de/zmt/output/LocationStayDurationsTest.java
+++ src/test/java/de/zmt/output/LocationStayDurationsTest.java
@@ -3,10 +3,12 @@ package de.zmt.output;
 import static javax.measure.unit.SI.SECOND;
 import static org.hamcrest.CoreMatchers.everyItem;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
@@ -22,12 +24,13 @@ import de.zmt.ecs.component.environment.HabitatMap;
 import de.zmt.output.LocationStayDurations.Headers;
 import de.zmt.util.Habitat;
 import de.zmt.util.TimeOfDay;
+import de.zmt.util.UnitConstants;
 import sim.field.grid.DoubleGrid2D;
 import sim.field.grid.IntGrid2D;
 import sim.util.Int2D;
 
 public class LocationStayDurationsTest {
-    private static final int HABITAT = Habitat.DEFAULT.ordinal();
+    private static final Habitat HABITAT = Habitat.DEFAULT;
     private static final double FOOD_DENSITY = 1;
     private static final long STEP_DURATION_SECOND = 1;
     /**
@@ -42,14 +45,16 @@ public class LocationStayDurationsTest {
 
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void obtainValues() {
-        Multimap<String, Object> valuesMap = create(2, 2).toMultimap();
+        Multimap<String, ?> valuesMap = create(2, 2).toMultimap();
 
         assertThat(valuesMap.get(Headers.CELL_X), containsInAnyOrder(0, 0, 1, 1));
         assertThat(valuesMap.get(Headers.CELL_Y), containsInAnyOrder(0, 0, 1, 1));
-        assertThat(valuesMap.get(Headers.HABITAT), everyItem(is(Habitat.DEFAULT)));
-        assertThat(valuesMap.get(Headers.FOOD_DENSITY), everyItem(is(FOOD_DENSITY)));
+        assertThat((Collection<Habitat>) valuesMap.get(Headers.HABITAT), everyItem(is(HABITAT)));
+        assertThat((Collection<Double>) valuesMap.get(Headers.FOOD_DENSITY), everyItem(
+                closeTo(FOOD_DENSITY + HABITAT.getFoodDensityMin().doubleValue(UnitConstants.FOOD_DENSITY), 1E-14d)));
     }
 
     @Test
@@ -66,7 +71,7 @@ public class LocationStayDurationsTest {
     }
 
     private static LocationStayDurations create(int width, int height) {
-        HabitatMap habitatMap = new HabitatMap(new IntGrid2D(width, height, HABITAT));
+        HabitatMap habitatMap = new HabitatMap(new IntGrid2D(width, height, HABITAT.ordinal()));
         FoodMap foodMap = new FoodMap(new DoubleGrid2D(width, height, FOOD_DENSITY), null);
         return new LocationStayDurations(Amount.valueOf(STEP_DURATION_SECOND, SECOND), habitatMap, foodMap);
     }
