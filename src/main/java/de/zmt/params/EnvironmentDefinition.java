a9d3a17035510802e783e9e64f817ba232527571
mkr <mkr@xy.net>
2019-06-26 10:01:44 +0200

    mkr - adjust natural mortality and predationRiskFactors according to gust et al 2002

--- src/main/java/de/zmt/params/EnvironmentDefinition.java
+++ src/main/java/de/zmt/params/EnvironmentDefinition.java
@@ -83,7 +83,7 @@ public class EnvironmentDefinition extends BaseParamDefinition
      * The maximum number of agents allowed. Larvae will not be created if
      * beyond this count.
      */
-    private int maxAgentCount = 250;
+    private int maxAgentCount = 200;
 
     /** Interval in simulation time for writing population data to file. */
     private Amount<Duration> outputPopulationInterval = Amount.valueOf(1, DAY).to(UnitConstants.SIMULATION_TIME);
